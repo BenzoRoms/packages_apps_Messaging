0ff4c0dc566fb7c82992a1656798aee58e2336a7
Keith Mok <kmok@cyngn.com>
2016-07-15 08:28:19 -0700

    Messaging: fix cursor not close explicitly
    Cursor always need to be close after query.
    
    FEIJ-1585
    
    Change-Id: Ie523c89b6271a28d3d2d8a08232387c1bd609880
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/messaging/sms/MmsUtils.java
+++ src/com/android/messaging/sms/MmsUtils.java
@@ -1445,10 +1445,10 @@ public class MmsUtils {
         final Cursor c = SqliteWrapper.query(context, context.getContentResolver(),
                 Mms.Inbox.CONTENT_URI, projection, selection,
                 new String[]{String.valueOf(threadId)}, null);
+        if (c == null) {
+            return;
+        }
         try {
-            if (c == null || c.getCount() == 0) {
-                return;
-            }
             while (c.moveToNext()) {
                 Uri uri = ContentUris.withAppendedId(Mms.CONTENT_URI, c.getLong(0));
                 String from = MmsUtils.getMmsSender(recipients, uri.toString());
@@ -1457,9 +1457,7 @@ public class MmsUtils {
                         self.getNormalizedDestination(), c.getString(1), status);
             }
         } finally {
-            if (c != null) {
-                c.close();
-            }
+            c.close();
         }
 
     }
